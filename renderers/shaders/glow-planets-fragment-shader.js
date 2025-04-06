da4a2a0d11a1e81aa690f8285b71cd02d25f33cb
Jim Kang <jimkang@gmail.com>
2025-04-05 21:46:35 -0400

    Halos.

--- renderers/shaders/glow-planets-fragment-shader.js
+++ renderers/shaders/glow-planets-fragment-shader.js
@@ -13,36 +13,37 @@ float distSquared(vec2 center, float radius, vec2 st) {
     return dot(distVec, distVec);
 }
 
-float isInCircle(vec2 center, float radius, float fuzzThickness, vec2 st) {
-    float distSquared = distSquared(center, radius, st);
-    return smoothstep(distSquared, distSquared + fuzzThickness, radius * radius);
+float isInCircle(vec2 center, float radius, float halo, float fuzzThickness, vec2 st) {
+    float distSq = distSquared(center, radius, st);
+    distSq = max(.5 * distSq, .5 * distSquared(center, radius + halo, st));
+    return smoothstep(distSq, distSq + fuzzThickness, radius * radius);
 }
 
-vec3 circleColor(vec2 center, float radius, vec3 baseColor, vec2 st) {
+vec3 circleColor(vec2 center, float radius, float halo, vec3 baseColor, vec2 st) {
     float pct = 0.0;
-    pct = isInCircle(center, radius, 0.005, st);
+    pct = isInCircle(center, radius, halo, 0.005, st);
     
     float colorPart = 1. - distSquared(center, radius, st)/(radius * radius);
 
     return pct * colorPart * baseColor;    
 }
 
-vec3 revolvingGlowBallColor(vec2 revolutionCenter, float revolutionRadius, float revolveRate, float radius, vec3 baseColor, vec2 st) {
+vec3 revolvingGlowBallColor(vec2 revolutionCenter, float revolutionRadius, float revolveRate, float radius, float halo, vec3 baseColor, vec2 st) {
     float angle = mod(u_time * revolveRate, 2. * PI);
     vec2 circleCenter = revolutionCenter + vec2(cos(angle), sin(angle)) * 0.3;
     // Radius pulse
     float pulseSpeed = 8.;
     radius *= cos(pulseSpeed * mod(u_time, 2. * PI))/16. + 15./16.;
-    return circleColor(circleCenter, radius, baseColor, st); 
+    return circleColor(circleCenter, radius, halo, baseColor, st); 
 }
 
 void main(){
 	vec2 st = gl_FragCoord.xy/u_resolution;
     
-    vec3 color = revolvingGlowBallColor(vec2(0.5), .3, 1., 0.194, vec3(.4, .3, 1.), st);
-    color += revolvingGlowBallColor(vec2(0.5, 0.5), .4, 1.25, 0.1, vec3(.4, .8, .2), st);
-    color += revolvingGlowBallColor(vec2(0.6, 0.35), .5, -.5, 0.1, vec3(.7, .2, .15), st);
+    vec3 color = revolvingGlowBallColor(vec2(.5), .3, 1., .4, .194, vec3(.4, .3, 1.), st);
+    color += revolvingGlowBallColor(vec2(.5, .5), .4, 1.25, .1, 0., vec3(.4, .8, .2), st);
+    color += revolvingGlowBallColor(vec2(.6, .35), .5, -.5, .2, .6, vec3(.7, .2, .15), st);
     
-	outColor = vec4(color, 1.0 );
+	outColor = vec4(color, 1.);
 }
 `;
