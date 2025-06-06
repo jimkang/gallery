939ba2b8873d2550f8dc87f15879c41098faf651
Jim Kang <jimkang@gmail.com>
2025-06-06 16:17:49 -0400

    Factor out renderPieces.

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import renderMovingMondrian from './renderers/render-moving-mondrian';
 import renderGlowPlanets from './renderers/render-glow-planets';
 import renderRandomCells from './renderers/render-random-cells';
 import renderMoteGhosts from './renderers/render-mote-ghosts';
-import renderPieceControls from './renderers/render-piece-controls';
+import renderPieces from './renderers/render-pieces';
 import RandomId from '@jimkang/randomid';
 import { URLStore } from '@jimkang/url-store';
 
@@ -41,61 +41,12 @@ function onUpdate({ seed, focusPiece, showWIP }) {
     return;
   }
 
-  var filteredPieceDefs = pieceDefs.filter((def) => showWIP || !def.wip);
-
-  if (focusPiece) {
-    let piece = filteredPieceDefs.find((def) => def.id === focusPiece);
-    showPiece({
-      piece,
-      seed,
-      maximize: true,
-    });
-  } else {
-    for (let piece of filteredPieceDefs) {
-      showPiece({ piece, seed });
-    }
-  }
-}
-
-function showPiece({ piece, seed, maximize = false }) {
-  let container = document.getElementById(piece.id + '-piece');
-  let canvas = document.getElementById(piece.id + '-canvas');
-
-  sizeCanvasToContainer({ container, canvas, maximize });
-  var resizeObserver = new ResizeObserver(onResizePieceContainer);
-  resizeObserver.observe(container as HTMLElement);
-
-  piece.renderer({ canvas, seed });
-  renderPieceControls({ piece: piece.id, urlStore });
-}
-
-function onResizePieceContainer(resizedEntries) {
-  for (let entry of resizedEntries) {
-    let container = entry.target;
-    let canvas = container.querySelector('canvas');
-    sizeCanvasToContainer({
-      container,
-      canvas,
-      maximize: container.classList.contains('maximized'),
-    });
-  }
-}
-
-function sizeCanvasToContainer({ container, canvas, maximize }) {
-  if (!canvas || !container) {
-    return;
-  }
-
-  container.classList[maximize ? 'add' : 'remove']('maximized');
-
-  var rect = container.getBoundingClientRect();
-  const squareSideLength = '' + Math.min(rect.width, rect.height);
-  resizeCanvas({ canvas, width: squareSideLength, height: squareSideLength });
-}
-
-function resizeCanvas({ canvas, width, height }) {
-  canvas.setAttribute('width', width);
-  canvas.setAttribute('height', height);
+  renderPieces({
+    pieceDefs: pieceDefs.filter((def) => showWIP || !def.wip),
+    focusPiece,
+    seed,
+    urlStore,
+  });
 }
 
 function reportTopLevelError(event: ErrorEvent) {
